b7c5a06d751de02e8413235eea5c8d4641993491
Jin Minjia <35626067+bluein-green@users.noreply.github.com>
2019-11-05 23:44:50 +0800

    extract features for large dataset (#9)
    * extract features for large dataset
    
    * edit to include newest features, create new large dataset

---
+++ Scripts/DataCleaning/extract_data_and_concat_large.py
@@ -0,0 +1,60 @@
+"""
+Goal of this script: extract relevant columns, concatenate label for each instance (located in another file)
+"""
+
+import jsonlines
+
+data = {}
+
+# extract data
+with jsonlines.open('instances_large.jsonl') as reader:
+    for obj in reader:
+        instance_id = obj["id"]
+        title = obj["targetTitle"]
+        description = obj["targetDescription"]
+        content = obj['targetParagraphs']
+        content = " ".join(content)
+        keywords = obj["targetKeywords"]
+
+        data[instance_id] = {
+                "id" : instance_id,
+                "title" : title,
+                "description" : description,
+                "content" : content,
+                "keywords" : keywords
+                }
+
+# extract truth values
+with jsonlines.open('truth.jsonl') as reader:
+    for obj in reader:
+        instance_id = obj["id"]
+        truthMean = obj["truthMean"]
+        truthMedian = obj["truthMedian"]
+        truthMode = obj["truthMode"]
+        truthClass = obj["truthClass"]
+        label = 0
+
+        # note that we indicate '1' if truthClass is clickbait
+        if truthClass == "clickbait":
+            label = 1
+
+        data[instance_id]["truthMean"] = truthMean
+        data[instance_id]["truthMedian"] = truthMedian
+        data[instance_id]["truthMode"] = truthMode
+        data[instance_id]["label"] = label
+
+"""
+for key in data:
+    print(key)
+    for obj in data[key]:
+        print(obj, ":", data[key][obj])
+    #print("id: %s; content: %s" % (key, value))
+"""
+
+output_file = open("instances_extracted_large.jsonl", "w")
+with jsonlines.Writer(output_file) as writer:
+    for key in data:
+        writer.write(data[key])
+output_file.close()
+
+
