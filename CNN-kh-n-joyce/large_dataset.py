cc721ca511f4db8a2b448e0b258feb0aa4b267a8
Kian Hwee <kianhwee_lim@yahoo.com.sg>
2019-11-05 15:06:22 +0800

    added the ability to choose the number of training instances

--- CNN-kh-n-joyce/large_dataset.py
+++ CNN-kh-n-joyce/large_dataset.py
@@ -7,7 +7,7 @@ import os
 import pandas as pd
 
 # to read extracted data from jsonl file
-def read_data(fileName):
+def read_data(fileName, cutoff = None):
     data = [["", "id", "title", "description", "content", "keywords", "truthMean", "truthMedian", "truthMode", "label"]]
     count = 1
     
@@ -26,14 +26,20 @@ def read_data(fileName):
             instance = [count, instance_id, title, description, content, keywords, truthMean, truthMedian, truthMode, label]
             data.append(instance)
             count += 1
+            if cutoff != None:
+                if count > cutoff:
+                    break
 
     data = np.array(data)
     return data
 
 # to get data packaged as pandas dataframe
-def get_data(fileName):
+def get_data(fileName, cutoff = None):
     # gets raw data as a np array
-    data_raw = read_data(fileName)
+    if cutoff:
+        data_raw = read_data(fileName, cutoff)
+    else:
+        data_raw = read_data(fileName)
 
     # convert np array into pandas dataframe
     data = pd.DataFrame(data=data_raw[1:, 1:],
@@ -42,11 +48,13 @@ def get_data(fileName):
 
     return data
 
-def get_large_dataset():
+def get_large_dataset(cutoff = None):
     parent_dir = os.path.dirname(os.getcwd())
     grandparent_dir = os.path.dirname(parent_dir)
 
     fileName = os.path.join(parent_dir, "InitialDataset/instances_extracted_large.jsonl")
+    if cutoff:
+        return get_data(fileName, cutoff = cutoff)
     return get_data(fileName)
 
 
